0622bfc9099334ed4a637ed6463fb74e3f4d4e40
amangit1314 <gitaman8481@gmail.com>
2022-01-16 12:22:57 +0530

    project created and base clinet initialized

---
+++ lib/controller/base_controller.dart
@@ -0,0 +1,27 @@
+
+import 'package:instagram_clone/helper/dialog_helper.dart';
+import 'package:instagram_clone/services/app_exceptions.dart';
+
+class BaseController {
+  void handleError(error) {
+    hideLoading();
+    if (error is BadRequestException) {
+      var message = error.message;
+      DialogHelper.showErroDialog(description: message);
+    } else if (error is FetchDataException) {
+      var message = error.message;
+      DialogHelper.showErroDialog(description: message);
+    } else if (error is ApiNotRespondingException) {
+      DialogHelper.showErroDialog(
+          description: 'Oops! It took longer to respond.');
+    }
+  }
+
+  showLoading(String message) {
+    DialogHelper.showLoading(message);
+  }
+
+  hideLoading() {
+    DialogHelper.hideLoading();
+  }
+}
---
+++ lib/controller/test_controller.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+// import 'package:http/http.dart' as http;
+import 'package:instagram_clone/controller/base_controller.dart';
+import 'package:instagram_clone/services/base_client.dart';
+
+class TestController extends GetxController with BaseController {
+  void getData() async {
+    showLoading('Fetching data');
+    var response = await BaseClient()
+        .get('https://jsonplaceholder.typicode.com', '/todos/1')
+        .catchError(handleError);
+    if (response == null) return;
+    hideLoading();
+    print(response);
+  }
+
+  void postData() async {
+    var request = {'message': 'CodeX sucks!!!'};
+    showLoading('Posting data...');
+    var response = await BaseClient()
+        .post('https://jsonplaceholder.typicode.com', '/posts', request)
+        .catchError(handleError);
+    if (response == null) return;
+    hideLoading();
+    print(response);
+  }
+}
